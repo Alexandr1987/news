bfff58bdf3ed69e198f6d08629272f7ba6e4d80c
Alexandr <advokat-1987@yandex.ru>
2015-09-27 22:16:49 +0300

    изменил add.php

--- www/controler/add.php
+++ www/controler/add.php
@@ -5,3 +5,13 @@
  * Date: 26.09.2015
  * Time: 22:38
  */
+require __DIR__.'/../function.php';
+//news_upload($name,$text);
+require __DIR__.'/../modules/sql.php';
+
+$name = $_POST['title_news'];
+$text = $_POST['text_news'];
+$date = date("F j, Y, g:i a");
+rec($name,$text);
+
+header('location: ../views/index.php');
