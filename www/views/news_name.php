7e1589c1bcd61eb448fc40448f6239a0859a04c4
Alexandr <advokat-1987@yandex.ru>
2015-09-27 22:17:59 +0300

    внес дату

--- www/views/news_name.php
+++ www/views/news_name.php
@@ -1,7 +1,26 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Александр
- * Date: 27.09.2015
- * Time: 0:37
- */
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>new_news</title>
+    <?php require __DIR__.'/../function.php';?>
+    <?php require __DIR__.'/../modules/sql.php';?>
+</head>
+<body>
+<?php $id = get_id();?>
+<?php $newses = get_sql();?>
+<?php foreach ($newses as $key => $value):?>
+    <?php if ($id == $value['id']):?>
+        <h2><?php echo $value['title'];?></h2>
+        <p><?php echo $value['text'];?></p>
+        <p><?php echo $value['date'];?></p>
+    <?php endif; ?>
+
+<?php endforeach; ?>
+</body>
+</html>
+
+
+
+
+
