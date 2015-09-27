2ec516caa01335213345df3f9f2a119d8805f646
Alexandr <advokat-1987@yandex.ru>
2015-09-27 22:18:10 +0300

    внес дату

--- www/function.php
+++ www/function.php
@@ -5,14 +5,24 @@
  * Date: 24.09.2015
  * Time: 21:19
  */
-function File_upload($field){
-    if (empty($_FILES))
-        return false;
-    if (0 != $_FILES[$field]['error'])
-        return false;
-    if (is_uploaded_file($_FILES[$field]['tmp_name']))
-        move_uploaded_file($_FILES[$field]['tmp_name'],
-            __DIR__.'/../news/'. $_FILES[$field]['name']);
-    return $rec = basename($_FILES[$field]['name']);
 
+$name =$_POST['title_news'];
+$text = $_POST['text_news'];
+/*
+function news_upload($name,$text){
+    if (empty($name))
+        return false;
+    if (empty($text))
+        return false;
+    if ( file_put_contents($name, $text)== true)
+        rename($name, '../text/'.$name);
+
+}
+*/
+function get_id()
+{
+    if (isset($_GET['id'])) {
+        $id = (int)$_GET['id'];
+    }
+return $id;
 }
